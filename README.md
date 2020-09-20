d6b9a8de7d89a52bf9b295b5d94feef6fa473187
Bohdan Chupika <chupika.bohdan@ukr.net>
2020-09-20 15:04:06 +0300

    added CommentDao test cases

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 1. There are two classes: Comment and Smile. One comment can have multiple smiles. 
 Implement the `remove(Comment comment)` method. Smiles used in this comment should NOT be removed.
+Add correct cascades. When you are creating a new comment you should use existed smiles (e.g. DON'T create a smile if it does not exist in DB)
 
 1. There are two classes: User and Comment. One user can have multiple comments. 
 Implement the `remove(User user)` method. Do not remove the user from DB. Mark him as removed. 
--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,49 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return entity;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert Content entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        return factory.openSession().get(Comment.class, id);
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaQuery<Comment> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Comment.class);
+            criteriaQuery.from(Comment.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get the list of smiles", e);
+        }
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction(); // Think we we do need it?
+        session.remove(entity);
+        transaction.commit();
+        session.close();
     }
 }
