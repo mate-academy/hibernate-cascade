53e9e90dab8cdd616c651a964fab3cd2d523d025
Leonid <rommel@ukr.net>
2020-10-02 00:15:36 +0300

    AbstractDao refactored

--- src/main/java/core/basesyntax/dao/impl/AbstractDao.java
+++ src/main/java/core/basesyntax/dao/impl/AbstractDao.java
@@ -11,15 +11,15 @@ public abstract class AbstractDao<T> {
         this.factory = sessionFactory;
     }
 
-    protected T create(T comment) {
+    protected T create(T item) {
         Session session = null;
         Transaction transaction = null;
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.persist(comment);
+            session.persist(item);
             transaction.commit();
-            return comment;
+            return item;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
@@ -33,10 +33,22 @@ public abstract class AbstractDao<T> {
     }
 
     public void remove(T item) {
-        try (Session session = factory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
             session.remove(item);
             transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't delete Content entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 }
