f68a4670901c136b95d98731be3b7b4b824d14df
jlightgreen <ShvabovichJulia@gmail.com>
2020-10-01 17:28:44 +0300

    hb-2

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetailsDao {
     public MessageDetailsDaoImpl(SessionFactory sessionFactory) {
@@ -10,12 +12,36 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
     }
 
     @Override
-    public MessageDetails create(MessageDetails entity) {
-        return null;
+    public MessageDetails create(MessageDetails details) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(details);
+            transaction.commit();
+            return details;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't create message details", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public MessageDetails get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            MessageDetails details = session.get(MessageDetails.class, id);
+            session.flush();
+            return details;
+        } catch (Exception e) {
+            throw new RuntimeException("Couldn't get message details", e);
+        }
     }
 }
