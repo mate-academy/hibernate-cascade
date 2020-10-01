4f6212e7ed6de79b2a20f4c0e44a1f0304f59b5f
Kostian <pyteec.ua@i.ua>
2020-10-01 17:38:54 +0300

    just save

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,8 +1,13 @@
 package core.basesyntax.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private List<Comment> comments;
