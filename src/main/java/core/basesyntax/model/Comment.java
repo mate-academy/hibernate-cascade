2d55a554b790f9693c1932a6143f2dad752ee332
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-10-03 01:57:39 +0300

    removed redundant import

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -2,7 +2,6 @@ package core.basesyntax.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
