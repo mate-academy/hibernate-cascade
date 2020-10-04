ace4ec339a2dc62170af8a92ad3a56f1d7911813
Borisko130 <krimenyukb@gmail.com>
2020-10-04 21:00:24 +0300

    Removed unnecessary empty lines

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -15,7 +15,6 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-
     @OneToMany
     private List<Smile> smiles;
 
