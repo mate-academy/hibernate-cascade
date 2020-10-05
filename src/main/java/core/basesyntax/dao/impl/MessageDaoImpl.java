7d986a36ce085fed08199b49b7c8f1672cf5532e
Martynice <marticiy@ukr.net>
2020-10-05 13:22:14 +0300

    renamed entity to original variables names

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -14,20 +14,20 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
     }
 
     @Override
-    public Message create(Message entity) {
+    public Message create(Message message) {
         Transaction transaction = null;
         Session session = null;
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.persist(entity);
+            session.persist(message);
             transaction.commit();
-            return entity;
+            return message;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Couldn't insert entity", e);
+            throw new RuntimeException("Couldn't insert message", e);
         } finally {
             if (session != null) {
                 session.close();
@@ -55,19 +55,19 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
     }
 
     @Override
-    public void remove(Message entity) {
+    public void remove(Message message) {
         Transaction transaction = null;
         Session session = null;
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.remove(entity);
+            session.remove(message);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Couldn't remove entity", e);
+            throw new RuntimeException("Couldn't remove message", e);
         } finally {
             if (session != null) {
                 session.close();
--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -12,20 +12,20 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
     }
 
     @Override
-    public MessageDetails create(MessageDetails entity) {
+    public MessageDetails create(MessageDetails messageDetails) {
         Transaction transaction = null;
         Session session = null;
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.persist(entity);
+            session.persist(messageDetails);
             transaction.commit();
-            return entity;
+            return messageDetails;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Couldn't insert entity", e);
+            throw new RuntimeException("Couldn't insert messageDetails", e);
         } finally {
             if (session != null) {
                 session.close();
