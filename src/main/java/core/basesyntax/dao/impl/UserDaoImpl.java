49f67a512fa618174d798c0f87a72c116be9098f
TimoxaV <vadim2493@gmail.com>
2020-10-05 13:26:35 +0300

    fixed missed Runtime in catch block

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -65,6 +65,7 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
+            throw new RuntimeException("Can't remove User entity", e);
         } finally {
             if (session != null) {
                 session.close();
