19b71dcc11d410d8da87c27ec72cf717e6e556e0
ROMASHSX <andriiromash@gmail.com>
2020-10-05 11:41:09 +0300

    travis fixed

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,7 +1,6 @@
 package core.basesyntax.model;
 
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
