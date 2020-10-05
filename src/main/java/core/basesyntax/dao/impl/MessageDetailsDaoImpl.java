95237831b757e3192156a3c079e80f6f1bdf4a08
Andriy <andriy.lagun@gmail.com>
2020-10-05 10:58:19 +0300

    reformatting for travis

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -4,7 +4,8 @@ import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
 import org.hibernate.SessionFactory;
 
-public class MessageDetailsDaoImpl extends AbstractDao<MessageDetails> implements MessageDetailsDao {
+public class MessageDetailsDaoImpl
+        extends AbstractDao<MessageDetails> implements MessageDetailsDao {
     public MessageDetailsDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,7 +1,12 @@
 package core.basesyntax.model;
 
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "comments")
