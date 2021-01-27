fcab4c2db78ea5b3dba723dc3a75862d8eaa6a23
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2021-01-27 17:52:19 +0200

    Fixed

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -25,7 +25,7 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't create message " + entity, e);
+            throw new RuntimeException("Can't create message details " + entity, e);
         } finally {
             if (session != null) {
                 session.close();
