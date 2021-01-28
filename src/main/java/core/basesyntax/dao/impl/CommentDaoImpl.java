d22c3dd3bdc2a7c67a34a0550cfcee5c52f246b7
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-28 10:58:15 +0200

    Update src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
    Co-authored-by: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,7 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.model.Comment;
 import org.hibernate.SessionFactory;
 
-public class CommentDaoImpl extends AbstractDao<Comment> {
+public class CommentDaoImpl extends AbstractDao<Comment> implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
