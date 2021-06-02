885905ef844b741e1cc25c09dc6453a01c63e8df
IhorRome <ihor.rome@gmail.com>
2021-06-02 13:54:21 +0300

    basic solution added

--- src/main/java/core/basesyntax/HibernateUtil.java
+++ src/main/java/core/basesyntax/HibernateUtil.java
@@ -4,10 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory = initSessionFactory();
-
-    private HibernateUtil() {
-    }
+    private static final SessionFactory sessionFactory = initSessionFactory();
 
     private static SessionFactory initSessionFactory() {
         try {
