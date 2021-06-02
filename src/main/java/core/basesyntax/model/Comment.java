8a8ecf4e97b9dd8617247d1ce18dd250a0798152
unknown <vovazalli@gmail.com>
2021-06-02 15:20:37 +0300

    Solution 1.0

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,37 +1,42 @@
 package core.basesyntax.model;
 
 import java.util.List;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    
+    @OneToMany
     private List<Smile> smiles;
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getContent() {
         return content;
     }
-
+    
     public void setContent(String content) {
         this.content = content;
     }
-
+    
     public List<Smile> getSmiles() {
         return smiles;
     }
-
+    
     public void setSmiles(List<Smile> smiles) {
         this.smiles = smiles;
     }
