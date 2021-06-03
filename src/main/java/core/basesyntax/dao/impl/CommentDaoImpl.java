c8678d5b2adaa41425dfbe4ff46afcdae6430400
Никита Фоминых <nikitafominyh@MacBook-Pro-Nikita.local>
2021-06-03 13:07:25 +0300

    fixed used method in create

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -20,7 +20,7 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.save(entity);
+            session.persist(entity);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
