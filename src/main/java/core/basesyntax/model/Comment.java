2a29ce07b23d424375e979a69700f7fb3540264c
Darina <to.darina.to>
2021-06-03 10:31:20 +0300

    mentor review1 done

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -2,7 +2,6 @@ package core.basesyntax.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,7 +13,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-    @OneToMany(fetch = FetchType.EAGER)
+    @OneToMany()
     private List<Smile> smiles;
 
     public Long getId() {
