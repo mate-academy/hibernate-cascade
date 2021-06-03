9078141b2dd637d8baf1c60345f82b5f440ab8ed
Anatolii Zarytskyi <tolia.zaritskiy@gmail.com>
2021-06-03 11:08:42 +0300

    fixed

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Messages_details")
+@Table(name = "messages_details")
 public class MessageDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
