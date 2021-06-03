85bfb6e3d68bf4ac43c1294d37df6c67433b86ca
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-06-03 12:45:44 +0300

    added some changes

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -61,7 +61,7 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.delete(entity);
+            session.remove(entity);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
