788edabb3151b8ede2fe15ca8d8326c48de9a4d6
aaananas <anastasiia.len11@gmail.com>
2021-08-03 13:48:00 +0200

    fixed model classes, added annotation

--- src/main/java/core/basesyntax/model/Smile.java
+++ src/main/java/core/basesyntax/model/Smile.java
@@ -5,8 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "smiles")
 public class Smile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
