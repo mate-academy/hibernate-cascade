200cb25a125e00f75ef54793842ea8cddb6906ce
mishamolnar <michaellmuller423@gmail.com>
2021-09-28 15:07:12 +0300

    build success

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -3,12 +3,9 @@ package core.basesyntax.model;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
