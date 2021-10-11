4cf3ab7c68b7c80e63f4206489e6a228af45eea4
Kostia Dementiev <konstde00@gmail.com>
2021-10-11 12:29:46 +0300

    fixed all problems

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -19,7 +19,7 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.save(entity);
+            session.persist(entity);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
