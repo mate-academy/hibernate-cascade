95a5534e521fe87a09248e5921878d326997046b
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-10-12 04:32:35 +0300

    sr v1 checklist

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.MessageDao;
 import core.basesyntax.model.Message;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,65 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
 
     @Override
     public Message create(Message entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create message: " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Message.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get message by Id: " + id, e);
+        }
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        String hql = "FROM Message";
+        try (Session session = factory.openSession()) {
+            Query query = session.createQuery(hql);
+            List<Message> messagesList = query.list();
+            return messagesList;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all the messages", e);
+        }
     }
 
     @Override
     public void remove(Message entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.delete(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't delete message: " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,8 +1,29 @@
 package core.basesyntax.model;
 
+import static org.hibernate.annotations.CascadeType.PERSIST;
+import static org.hibernate.annotations.CascadeType.REMOVE;
+import static org.hibernate.annotations.CascadeType.SAVE_UPDATE;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+
+    @Cascade({PERSIST, SAVE_UPDATE, REMOVE})
+    @OneToMany(fetch = FetchType.EAGER)
     private List<MessageDetails> messageDetails;
 
     public Long getId() {
