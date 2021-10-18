d0023687348bdfc1777b8a8209a238c43a898e41
Serhii Orlov <orlov30@gmail.com>
2021-10-18 21:36:40 +0300

    change relation type.

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -7,7 +7,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +17,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-    @OneToMany(cascade = CascadeType.REFRESH,
+    @ManyToMany(cascade = CascadeType.REFRESH,
             fetch = FetchType.EAGER)
     private List<Smile> smiles;
 
