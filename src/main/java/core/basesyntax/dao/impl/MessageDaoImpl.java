28fe78d629e2a4083921a4e099d3ae583158833a
Slava Cheban <37748459+annaRav@users.noreply.github.com>
2021-11-08 13:33:56 +0200

    Update src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
    Co-authored-by: Dasha <maliuk.dasha@gmail.com>

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -68,7 +68,7 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw e;
+            throw new RuntimeException("Cant remove message " + entity, e);
         } finally {
             if (session != null) {
                 session.close();
