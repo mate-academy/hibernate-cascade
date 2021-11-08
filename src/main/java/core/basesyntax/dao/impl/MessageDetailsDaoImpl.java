8c740b245e46903901dc8c203f835d74efa465e8
Viacheslav Cheban <vcheb@eg.dk>
2021-11-08 11:24:12 +0200

    fix code style in MessageDetailsDaoImpl

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -25,7 +25,8 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can's save messageDetail do db, messageDetail " + entity, e);
+            throw new RuntimeException(
+                "Can's save messageDetail do db, messageDetail " + entity, e);
         } finally {
             if (session != null) {
                 session.close();
