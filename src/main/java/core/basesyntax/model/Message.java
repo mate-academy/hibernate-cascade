426792b88d9757eec6fc5eb05a6e15d61c3b38e8
Chupiak <chupyak@gmail.com>
2021-11-11 17:15:29 +0200

    1.0 Solution "hibernate = cascade"

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,8 +1,21 @@
 package core.basesyntax.model;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import org.hibernate.annotations.Cascade;
+
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToMany
+    @Cascade(value = org.hibernate.annotations.CascadeType.ALL)
     private List<MessageDetails> messageDetails;
 
     public Long getId() {
