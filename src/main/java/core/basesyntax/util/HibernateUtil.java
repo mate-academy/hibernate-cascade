fec744c638e0e698505474be9a96e0362faae0b5
Olha Tankevych <freakystyley85@gmail.com>
2021-12-22 15:42:08 +0200

    implemented all DAO interfaces

--- src/main/java/core/basesyntax/util/HibernateUtil.java
+++ src/main/java/core/basesyntax/util/HibernateUtil.java
@@ -1,10 +1,10 @@
-package core.basesyntax;
+package core.basesyntax.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory = initSessionFactory();
+    private static final SessionFactory sessionFactory = initSessionFactory();
 
     private HibernateUtil() {
     }
@@ -13,7 +13,7 @@ public class HibernateUtil {
         try {
             return new Configuration().configure().buildSessionFactory();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't configure SessionFactory", e);
         }
     }
 
