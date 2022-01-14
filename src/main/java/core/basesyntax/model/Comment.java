3a634b3b9bfd860d97ef396c9ad33f30206488d8
ASelivonchyk <aselivonchyk@gmail.com>
2022-01-14 16:36:08 +0200

    change annotation on field List<Smile> smiles at Comment.class to @ManyToMany;

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -6,7 +6,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,7 +16,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-    @OneToMany (fetch = FetchType.EAGER)
+    @ManyToMany (fetch = FetchType.EAGER)
     private List<Smile> smiles;
 
     public Long getId() {
