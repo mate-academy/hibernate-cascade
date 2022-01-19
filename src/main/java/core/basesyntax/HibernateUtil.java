caed5d96652d22486865e69ad2405fe145af5250
forsfakel <81535727+forsfakel@users.noreply.github.com>
2022-01-19 14:19:27 +0200

    Implemented all methods from DAO layer. Added annotations for entities to model. Added cascades.

--- src/main/java/core/basesyntax/HibernateUtil.java
+++ src/main/java/core/basesyntax/HibernateUtil.java
@@ -1,6 +1,12 @@
 package core.basesyntax;
 
+import core.basesyntax.model.Comment;
+import core.basesyntax.model.Message;
+import core.basesyntax.model.MessageDetails;
+import core.basesyntax.model.Smile;
+import core.basesyntax.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
@@ -11,7 +17,16 @@ public class HibernateUtil {
 
     private static SessionFactory initSessionFactory() {
         try {
-            return new Configuration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration().configure();
+            configuration.addAnnotatedClass(Comment.class);
+            configuration.addAnnotatedClass(Smile.class);
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Message.class);
+            configuration.addAnnotatedClass(MessageDetails.class);
+            StandardServiceRegistryBuilder builder
+                    = new StandardServiceRegistryBuilder().applySettings(
+                    configuration.getProperties());
+            return configuration.buildSessionFactory(builder.build());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
