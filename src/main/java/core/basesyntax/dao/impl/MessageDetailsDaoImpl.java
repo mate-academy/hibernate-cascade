aaccfc3a4073091f52980ebc1727111145209c24
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-20 21:18:39 +0200

    Implement MessageDetailsDaoImpl

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -2,7 +2,11 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.PersistenceException;
 
 public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetailsDao {
     public MessageDetailsDaoImpl(SessionFactory sessionFactory) {
@@ -11,11 +15,33 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
 
     @Override
     public MessageDetails create(MessageDetails entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (PersistenceException persistenceException) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't save message details: "
+                    + entity, persistenceException);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public MessageDetails get(Long id) {
-        return null;
+        MessageDetails messageDetails;
+        try (Session session = factory.openSession()) {
+            messageDetails = session.get(MessageDetails.class, id);
+        }
+        return messageDetails;
     }
 }
