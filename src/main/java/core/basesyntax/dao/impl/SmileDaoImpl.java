b8f1e94a9c43608084e953a28e7c327873d975d5
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-20 19:49:09 +0200

    Implement SmileDaoImpl

--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -3,7 +3,12 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import javax.persistence.PersistenceException;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
     public SmileDaoImpl(SessionFactory sessionFactory) {
@@ -12,16 +17,50 @@ public class SmileDaoImpl extends AbstractDao implements SmileDao {
 
     @Override
     public Smile create(Smile entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (PersistenceException persistenceException) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't save smile: "
+                    + entity, persistenceException);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        Smile smile;
+        try (Session session = factory.openSession()) {
+            smile = session.get(Smile.class, id);
+        } catch (PersistenceException persistenceException) {
+            throw new RuntimeException("Couldn't get smile by id: "
+                    + id, persistenceException);
+        }
+        return smile;
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        List<Smile> allSmiles;
+        try (Session session = factory.openSession()) {
+            Query<Smile> getAllSmilesQuery = session
+                    .createQuery("from Smile", Smile.class);
+            allSmiles = getAllSmilesQuery.getResultList();
+        } catch (PersistenceException persistenceException) {
+            throw new RuntimeException("Couldn't get all smiles from DB",
+                    persistenceException);
+        }
+        return allSmiles;
     }
 }
