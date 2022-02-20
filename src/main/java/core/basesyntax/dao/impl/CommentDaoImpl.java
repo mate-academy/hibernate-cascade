b657d62c1e4843efb28cd51909e6316d2676a9fe
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-20 20:04:37 +0200

    Implement CommentDaoImpl

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,13 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import javax.persistence.PersistenceException;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +18,71 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (PersistenceException persistenceException) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't save comment: "
+                    + entity, persistenceException);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        Comment comment;
+        try (Session session = factory.openSession()) {
+            comment = session.get(Comment.class, id);
+        }
+        return comment;
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        List<Comment> allComments;
+        try (Session session = factory.openSession()) {
+            Query<Comment> allCommentsQuery = session
+                    .createQuery("from Comment", Comment.class);
+            allComments = allCommentsQuery.getResultList();
+        } catch (PersistenceException persistenceException) {
+            throw new RuntimeException("Couldn't get all comments from DB",
+                    persistenceException);
+        }
+
+        return allComments;
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (PersistenceException persistenceException) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't remove comment: "
+                    + entity, persistenceException);
+        }  finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
