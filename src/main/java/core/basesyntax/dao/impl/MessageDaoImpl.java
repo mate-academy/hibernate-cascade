fd1bd412e8001faaa0032865126cbfd9f864c1ce
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-20 21:35:29 +0200

    Implement MessageDaoImpl

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -3,7 +3,13 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.MessageDao;
 import core.basesyntax.model.Message;
 import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import javax.persistence.PersistenceException;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +18,72 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
 
     @Override
     public Message create(Message entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (PersistenceException persistenceException) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't save message: "
+                    + entity, persistenceException);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        Message message;
+        try (Session session = factory.openSession()) {
+            message = session.get(Message.class, id);
+        } catch (PersistenceException persistenceException) {
+            throw new RuntimeException("Couldn't get message by id: "
+                    + id, persistenceException);
+        }
+        return message;
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        List<Message> allMessages;
+        try (Session session = factory.openSession()) {
+            Query<Message> allMessageQuery = session
+                    .createQuery("from Message", Message.class);
+            allMessages = allMessageQuery.getResultList();
+        } catch (PersistenceException persistenceException) {
+            throw new RuntimeException("Couldn't get all messages from DB",
+                    persistenceException);
+        }
+        return allMessages;
     }
 
     @Override
     public void remove(Message entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (PersistenceException persistenceException) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't remove message: "
+                    + entity, persistenceException);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
