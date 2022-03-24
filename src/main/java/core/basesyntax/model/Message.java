a4cef733a3ccc35518c63877c84354a64962e934
nastyaprokhorenko <nastyaprokhorenko527@gmail.com>
2022-03-25 00:30:05 +0200

    unused import was deleted

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -2,7 +2,6 @@ package core.basesyntax.model;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
