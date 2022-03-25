1758e4f9c99f53b7f5532087f4d2fa5869c87198
Daniil Opryshko <dvo8899@gmail.com>
2022-03-25 21:42:06 +0200

    added cascades

---
+++ src/main/java/core/basesyntax/exception/DataProcessingException.java
@@ -0,0 +1,18 @@
+package core.basesyntax.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException() {
+    }
+
+    public DataProcessingException(String message) {
+        super(message);
+    }
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataProcessingException(Throwable cause) {
+        super(cause);
+    }
+}
