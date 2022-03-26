3f285f686fcd053481e2024151900c738fab2354
Maksym Pokras <mxpokras@gmail.com>
2022-03-26 11:14:28 +0200

    List<Smile> annotated to reflect relation between comment and smile

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,15 +1,18 @@
 package core.basesyntax.model;
 
-import java.util.List;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
 
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @ManyToMany
     private List<Smile> smiles;
 
     public Long getId() {
