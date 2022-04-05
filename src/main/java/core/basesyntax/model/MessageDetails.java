039bd519524c76ab63bae9346cb28fe0306ddbfc
sltnsrh <lifenjoy8888@gmail.com>
2022-04-05 11:07:30 +0300

    hibernate cascade implementation

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,8 +1,15 @@
 package core.basesyntax.model;
 
 import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class MessageDetails {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String sender;
     private LocalDateTime sentTime;
