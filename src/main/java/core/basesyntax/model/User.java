8ed218ddc4f2154e30ce9fa1f68cc8f1587091f3
Oleh Mytiai <mytiai.ov@icloud.com>
2022-04-08 19:25:37 +0300

    implemented DAO classes, passed all tests

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -3,6 +3,7 @@ package core.basesyntax.model;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -16,7 +17,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
-    @OneToMany(cascade = CascadeType.PERSIST)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     private List<Comment> comments;
 
     public Long getId() {
