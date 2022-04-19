ca59881d631568d0fc72f4d43def865a96dfae39
Leonid <rommel@ukr.net>
2022-04-19 12:30:39 +0300

    Model changed

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,12 +1,11 @@
 package core.basesyntax.model;
 
-import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,7 +15,7 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL)
     private MessageDetails messageDetails;
 
     public Long getId() {
