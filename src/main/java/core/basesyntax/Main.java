4a07361be80b292ca31bf284a4f775a28f8f8e1a
Serhii Pankov <pankoff.serg@gmail.com>
2022-05-25 16:51:09 +0300

    add solution for hibernate-cascade

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -15,39 +15,31 @@ import org.hibernate.SessionFactory;
 public class Main {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-
         Smile smile = new Smile();
         smile.setValue("smile");
         SmileDao smileDao = new SmileDaoImpl(sessionFactory);
         smileDao.create(smile);
-
         Comment comment = new Comment();
         comment.setContent("comment");
         comment.setSmiles(List.of(smile));
-
         Comment comment2 = new Comment();
         comment2.setContent("comment2");
         comment2.setSmiles(List.of(smile));
-
         User user = new User();
         user.setUsername("User");
         user.setComments(List.of(comment));
-
         User user2 = new User();
         user2.setUsername("User2");
         user2.setComments(List.of(comment2));
-
         UserDao userDao = new UserDaoImpl(sessionFactory);
+
         userDao.create(user);
         userDao.create(user2);
-
         System.out.println(userDao.getAll());
-
         CommentDao commentDao = new CommentDaoImpl(sessionFactory);
         commentDao.remove(comment2);
         System.out.println(userDao.getAll());
         userDao.remove(user2);
-
         System.out.println(userDao.getAll());
     }
 }
