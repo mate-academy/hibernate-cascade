57d7d41c2416c604fd44e510eaaf2d26215a4f4f
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-06-01 14:47:24 +0300

    hibernate-cascade-hw Fomenko

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,5 +1,6 @@
 package core.basesyntax.model;
 
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import java.util.List;
 
 @Entity
 public class User {
