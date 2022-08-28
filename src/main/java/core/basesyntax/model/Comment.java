e694344410bcffbc0734836926c747b1f0c70734
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-08-29 01:40:52 +0300

    Third commit

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,7 +1,6 @@
 package core.basesyntax.model;
 
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
