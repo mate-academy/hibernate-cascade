ce921341dbcba5d7a777a4c4bbbb8ffec8ec71cb
Serhii Davydov <serhii.davydov@gmail.com>
2022-09-05 12:59:40 +0300

    First commit

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,10 +1,23 @@
 package core.basesyntax.model;
 
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     private List<Comment> comments;
 
     public Long getId() {
@@ -23,6 +36,7 @@ public class User {
         this.username = username;
     }
 
+    @OneToMany(cascade = CascadeType.PERSIST)
     public List<Comment> getComments() {
         return comments;
     }
