d31b513edbec3998fb748f1d202e0f38d038f131
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-09-09 22:35:24 +0300

    implemented interfaces in dao package

--- src/main/java/core/basesyntax/dao/MessageDao.java
+++ src/main/java/core/basesyntax/dao/MessageDao.java
@@ -1,6 +1,14 @@
 package core.basesyntax.dao;
 
 import core.basesyntax.model.Message;
+import java.util.List;
 
 public interface MessageDao extends GenericDao<Message> {
+    Message create(Message entity);
+
+    Message get(Long id);
+
+    List<Message> getAll();
+
+    void remove(Message entity);
 }
