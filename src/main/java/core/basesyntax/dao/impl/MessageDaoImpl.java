7b549c1d6b6551837e4fcd31fba6dd95bd093a1b
Anna Popova <anitapopova1996@gmail.com>
2022-09-27 19:42:39 +0300

    Implemented methods

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -36,16 +37,43 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
 
     @Override
     public Message get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Message.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get message with id: " + id, e);
+        }
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        String query = "SELECT u FROM Message u";
+        try (Session session = factory.openSession()) {
+            Query<Message> getAllUsersQuery = session.createQuery(
+                    query, Message.class);
+            return getAllUsersQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all Messages", e);
+        }
     }
 
     @Override
     public void remove(Message entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove this message: " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
