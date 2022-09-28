eed45057dfe72abbea0022aa41d4f9dcf02ba245
max-bolotin <realprof.mx@gmail.com>
2022-09-29 01:26:39 +0300

    All the required interfaces, classes and methods implemented

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetailsDao {
     public MessageDetailsDaoImpl(SessionFactory sessionFactory) {
@@ -10,12 +12,29 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
     }
 
     @Override
-    public MessageDetails create(MessageDetails entity) {
-        return null;
+    public MessageDetails create(MessageDetails messageDetails) {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(messageDetails);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save message details in DB", e);
+        }
+        return messageDetails;
     }
 
     @Override
     public MessageDetails get(Long id) {
-        return null;
+        MessageDetails messageDetails;
+        try (Session session = factory.openSession()) {
+            messageDetails = session.get(MessageDetails.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get message details with id=" + id + " from DB", e);
+        }
+        return messageDetails;
     }
 }
--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
     public SmileDaoImpl(SessionFactory sessionFactory) {
@@ -11,17 +14,40 @@ public class SmileDaoImpl extends AbstractDao implements SmileDao {
     }
 
     @Override
-    public Smile create(Smile entity) {
-        return null;
+    public Smile create(Smile smile) {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(smile);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save smile in DB", e);
+        }
+        return smile;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        Smile smile;
+        try (Session session = factory.openSession()) {
+            smile = session.get(Smile.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get smile with id=" + id + " from DB", e);
+        }
+        return smile;
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            Query<Smile> getAllSmilesQuery = session.createQuery(
+                    "from Smile ", Smile.class);
+            return getAllSmilesQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all smiles from DB", e);
+        }
     }
 }
