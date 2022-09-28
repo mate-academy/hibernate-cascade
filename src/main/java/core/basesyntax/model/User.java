07be464eef31b63946f01e7bb9439a6b3ce67cca
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-28 16:58:21 +0300

    Remove user from the DB. Comments left by this user should NOT be removed. Add correct cascades. When you are creating a new user you should use also create comments (save each new comment in the DB).

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,7 +1,9 @@
 package core.basesyntax.model;
 
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,7 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
-    @OneToMany
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     private List<Comment> comments;
 
     public Long getId() {
