4be4101ecd311cadcf25c3d3c6aa820e6eab0ee1
LubusUzh <lubus.uzh@gmail.com>
2022-09-28 22:38:24 +0300

    Add: imports

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,6 +1,10 @@
 package core.basesyntax.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
