03f9922bb04c215b608678e8675b51dca1168410
Ilja Nekrituk <Nekrit99@gmail.com>
2022-09-29 23:17:05 +0300

    done!

--- src/main/java/core/basesyntax/dao/SmileDao.java
+++ src/main/java/core/basesyntax/dao/SmileDao.java
@@ -3,10 +3,5 @@ package core.basesyntax.dao;
 import core.basesyntax.model.Smile;
 import java.util.List;
 
-public interface SmileDao {
-    Smile create(Smile smile);
-
-    Smile get(Long id);
-
-    List<Smile> getAll();
+public interface SmileDao extends GenericDao<Smile> {
 }
---
+++ src/main/java/core/basesyntax/exception/DataProcessingException.java
@@ -0,0 +1,8 @@
+package core.basesyntax.exception;
+
+public class DataProcessingException extends RuntimeException {
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
