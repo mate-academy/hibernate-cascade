c4af724563744fa133c86e3e74197a1e0bc03ca1
EagleAndrew <tortloveduck4@gmail.com>
2022-09-29 15:24:38 +0200

    code style

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,6 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
-
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -16,24 +15,24 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-            Session session = null;
-            Transaction transaction = null;
-            try {
-                session = factory.openSession();
-                transaction = session.beginTransaction();
-                session.save(entity);
-                transaction.commit();
-            } catch (Exception e) {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-                throw new RuntimeException("Can't save comment " + entity + " to DB", e);
-            } finally {
-                if (session != null) {
-                    session.close();
-                }
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
             }
-            return entity;
+            throw new RuntimeException("Can't save comment " + entity + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
