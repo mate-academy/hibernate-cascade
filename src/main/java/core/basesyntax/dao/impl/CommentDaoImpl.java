b56a037925d996a536fb5222fbbc21f14164ad76
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-29 16:21:52 +0300

    all methods in CommentDaoImpl were implemented

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -36,16 +36,40 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment get(Long id) {
-        return null;
+        try (Session session = factory.openSession()){
+            return session.get(Comment.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get comment from DB with id=" + id, e);
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = factory.openSession()){
+            return session.createQuery("from comments", Comment.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all comments from DB. ", e);
+        }
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.delete(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw  new RuntimeException("Can't remove comment " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
