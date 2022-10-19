0ce2ad1b660998f522ab9da49258a03194765685
Ihor Dmytrush <ihor.dm.a@gmail.com>
2022-10-19 16:09:22 +0300

    1st try

--- src/main/java/core/basesyntax/dao/impl/AbstractDao.java
+++ src/main/java/core/basesyntax/dao/impl/AbstractDao.java
@@ -1,11 +1,25 @@
 package core.basesyntax.dao.impl;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public abstract class AbstractDao {
     protected final SessionFactory factory;
 
     protected AbstractDao(SessionFactory sessionFactory) {
         this.factory = sessionFactory;
+    }
+
+    public void transactionRollBack(Transaction transaction) {
+        if (transaction != null) {
+            transaction.rollback();
+        }
+    }
+
+    public void sessionClose(Session session) {
+        if (session != null) {
+            session.close();
+        }
     }
 }
