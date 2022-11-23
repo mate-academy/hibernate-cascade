7cf3f024c06246d6d92fd8ca0b9bf141bac1c782
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-11-23 14:00:02 +0200

    Solution was added

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +14,62 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = super.factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create comment", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        try (Session session = super.factory.openSession()) {
+            return session.get(Comment.class, id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't find comment by id " + id);
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = super.factory.openSession()) {
+            return session.createQuery("SELECT c from Comment c", Comment.class).list();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get all comments from db");
+        }
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = super.factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove comment");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
