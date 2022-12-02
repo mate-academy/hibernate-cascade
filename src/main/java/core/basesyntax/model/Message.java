6d843334accb4c30d97face9dccc72efec5b9b94
Fokin <Fokin1@ukr.net>
2022-12-02 07:07:06 +0100

    fixed cascade type in User and Message

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,13 +1,15 @@
 package core.basesyntax.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 @Entity
 @Table(name = "maessage")
@@ -16,7 +18,8 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @OneToOne(fetch = FetchType.EAGER)
+    @Cascade(value = {CascadeType.SAVE_UPDATE, CascadeType.DELETE})
     @JoinColumn(name = "messageDetail_id")
     private MessageDetails messageDetails;
 
