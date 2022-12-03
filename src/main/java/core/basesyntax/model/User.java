ae1b0ff3f0d60b2b211d1dafebb8e0fcd18b7602
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-12-03 21:07:58 +0200

    Commit before electricity cutting off

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -3,9 +3,14 @@ package core.basesyntax.model;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
