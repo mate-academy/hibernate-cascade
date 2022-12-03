1d7b787a391034ad0613f9485c0cee6aa05f773b
Mark Rybnikov <mark.rybnikov@amdaris.com>
2022-12-03 21:12:45 +0200

    Task is completed

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -2,12 +2,14 @@ package core.basesyntax.model;
 
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
