b214067cfbeab09f7bc1d1b646116a9afa4254ef
DooMLeoN <levberezov01@gmail.com>
2022-12-09 16:47:24 +0200

    second commit

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,7 +16,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-    @ManyToMany(cascade = CascadeType.MERGE)
+    @OneToMany(cascade = CascadeType.MERGE)
     private List<Smile> smiles;
 
     public Long getId() {
