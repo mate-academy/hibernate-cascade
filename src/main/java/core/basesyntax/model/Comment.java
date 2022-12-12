d3640f72c4cca8fb18ff6a518587168ff1bdb0a1
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-12-12 10:18:57 +0200

    added annotations - Entity, GeneratedValue, Id, Cascade and other

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,15 +1,24 @@
 package core.basesyntax.model;
 
 import java.util.List;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
+@Entity
+@Table(name = "comments")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToMany
+    @Cascade(value = CascadeType.MERGE)
     private List<Smile> smiles;
 
     public Long getId() {
