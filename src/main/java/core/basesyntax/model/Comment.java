d88bcb42cfa5755ce3d2136f8cef7e2a68e43c30
Artem Voitiuk <artemon2504@gmail.com>
2023-02-13 22:33:35 +0200

    task solved using cascades 1 commit

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,15 +1,19 @@
 package core.basesyntax.model;
 
 import java.util.List;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToMany
     private List<Smile> smiles;
 
     public Long getId() {
