cbc243bb3b2258211e497b6fdc50066da7859118
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2023-02-25 21:51:48 +0200

    Implemented all DAO interfaces and added required annotations for entities.

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,7 +1,11 @@
 package core.basesyntax.model;
 
-import javax.persistence.*;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,7 +1,11 @@
 package core.basesyntax.model;
 
 import java.time.LocalDateTime;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "message_details")
