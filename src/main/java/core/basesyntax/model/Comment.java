9c0580f081a063b89368b2d94649069c653b7ac2
Dmytro Busyhin <zza4etik@gmail.com>
2023-02-26 22:45:40 +0200

    removes unused imports

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,13 +1,11 @@
 package core.basesyntax.model;
 
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
 public class Comment {
