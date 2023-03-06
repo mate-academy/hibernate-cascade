f9e22c29cdef32e060d2a8309e406a18682fc00d
vasyl.moshun <vasyl.moshun@gmail.com>
2023-03-06 14:04:32 +0200

    fixed

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "comments")
 public class Comment {
