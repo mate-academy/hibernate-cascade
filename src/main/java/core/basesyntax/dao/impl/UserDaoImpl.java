fc1431c6691d3a9fa15d33064753076a5b29eeea
Andrii Malenko <andmalenko@gmail.com>
2023-03-17 17:27:50 +0200

    Here is my solution for hw-cascade

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+import javax.persistence.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,67 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                throw new RuntimeException("Can't add a user to DB " + entity, e);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        User user = null;
+        try (Session entityManager = factory.openSession()) {
+            user = entityManager.find(User.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find user by id " + id, e);
+        }
+        return user;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User> userList = null;
+        try (Session session = factory.openSession()) {
+            Query getAllCommentsQuery = session.createQuery("from User ", User.class);
+            userList = getAllCommentsQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all users from DB", e);
+        }
+        return userList;
     }
 
     @Override
     public void remove(User entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove users from DB " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
