33fe5126a712e55177dee3b17784e8fc6d135f2b
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-03-28 15:43:56 +0300

    jv-hibernate-cascade

--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
     public SmileDaoImpl(SessionFactory sessionFactory) {
@@ -12,16 +15,45 @@ public class SmileDaoImpl extends AbstractDao implements SmileDao {
 
     @Override
     public Smile create(Smile entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create a smile: "
+                    + entity + " in db", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Smile.class, id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get smile", e);
+        }
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            Query<Smile> getAllProductsQuery
+                    = session.createQuery("from Smile", Smile.class);
+            return getAllProductsQuery.getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get all smiles from DB");
+        }
     }
 }
+
--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,9 +1,28 @@
 package core.basesyntax.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToOne
+    @Cascade(value = CascadeType.ALL)
     private MessageDetails messageDetails;
+
+    public Message() {
+
+    }
 
     public Long getId() {
         return id;
