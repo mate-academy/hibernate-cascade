caf561e0199133847bf4e717d590a9bc2b6ab594
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-04 20:46:42 +0300

    implement CommentDaoImpl

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,6 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import core.basesyntax.model.Smile;
 import org.hibernate.SessionFactory;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
@@ -12,21 +16,75 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = factory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add comment to DB " + entity, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        EntityManager entityManager = null;
+        try {
+            entityManager = factory.createEntityManager();
+            return entityManager.find(Comment.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find comment in DB by id: " + id, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        EntityManager entityManager = null;
+        try {
+            entityManager = factory.createEntityManager();
+            TypedQuery<Comment> getAllCommentsQuery = entityManager.createQuery("from Comment", Comment.class);
+            return getAllCommentsQuery.getResultList();
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void remove(Comment entity) {
-
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = factory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove comment from DB " + entity, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
