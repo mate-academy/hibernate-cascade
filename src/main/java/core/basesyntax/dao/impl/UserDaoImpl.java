8b7ebdc8d52b95c15cfcc67777853fa64b1a3c85
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-04 21:12:05 +0300

    implement UserDaoImpl

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,6 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import org.hibernate.SessionFactory;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
@@ -12,21 +15,77 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = factory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add user to DB: " + entity, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        EntityManager entityManager = null;
+        try {
+            entityManager = factory.createEntityManager();
+            return entityManager.find(User.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find user in DB by id: " + id, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        EntityManager entityManager = null;
+        try {
+            entityManager = factory.createEntityManager();
+            TypedQuery<User> getAllUsersQuery = entityManager.createQuery("from User", User.class);
+            return getAllUsersQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all users from DB");
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void remove(User entity) {
-
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = factory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove user from DB " + entity, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
