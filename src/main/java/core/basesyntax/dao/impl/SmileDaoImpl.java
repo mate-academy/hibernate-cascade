450f3e8cfefea8d3b8701e171e5d71d5493d2069
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-04-05 13:28:39 +0200

    hibernate home task cascade

--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
     public SmileDaoImpl(SessionFactory sessionFactory) {
@@ -12,16 +15,47 @@ public class SmileDaoImpl extends AbstractDao implements SmileDao {
 
     @Override
     public Smile create(Smile entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("transaction is null", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        Session session = null;
+        Smile smile = null;
+        try {
+            session = factory.openSession();
+            smile = session.get(Smile.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("can`t open session ", e);
+        } finally {
+            session.close();
+        }
+        return smile;
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            Query<Smile> getAllCommentsQuery = session.createQuery(
+                    "from Smile c", Smile.class);
+            return getAllCommentsQuery.getResultList();
+        }
     }
 }
