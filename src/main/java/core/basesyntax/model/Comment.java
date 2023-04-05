4d62cc7ce170d21a62a4042d13175992ccd72818
sasha <radovietsoleksandr@gmail.com>
2023-04-06 02:41:35 +0300

    w

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,15 +1,25 @@
 package core.basesyntax.model;
 
 import java.util.List;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
+@Entity
+@Table (name = "comment")
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+
+    @OneToMany
+    @Cascade({CascadeType.PERSIST})
     private List<Smile> smiles;
 
     public Long getId() {
@@ -34,5 +44,18 @@ public class Comment {
 
     public void setSmiles(List<Smile> smiles) {
         this.smiles = smiles;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{"
+                + "id="
+                + id
+                + ", content='"
+                + content
+                + '\''
+                + ", smiles="
+                + smiles
+                + '}';
     }
 }
