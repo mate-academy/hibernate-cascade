8463f69de6e30323b8f5f5fa8be0b05b60d8a13b
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-04-10 17:56:03 +0300

    Implemented DAO services, Added cascade annotations

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -2,6 +2,8 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import org.hibernate.SessionFactory;
 
 public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetailsDao {
@@ -11,11 +13,45 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
 
     @Override
     public MessageDetails create(MessageDetails entity) {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.openSession();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            entityManager.persist(entity);
+            entityTransaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Can't add message details to DB" + entity);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public MessageDetails get(Long id) {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.openSession();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            return entityManager.find(MessageDetails.class, id);
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Can't get message details from DB by id" + id);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,90 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.openSession();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            entityManager.persist(entity);
+            entityTransaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Can't add user to DB" + entity);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.openSession();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            return entityManager.find(User.class, id);
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Can't get user from DB by id" + id);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.openSession();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            final Query<User> getAllComments = (Query<User>) entityManager.createQuery(
+                    "from User", User.class);
+            return getAllComments.getResultList();
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Can't get all users from DB" + e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public void remove(User entity) {
-
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.openSession();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            entityManager.remove(entity);
+            entityTransaction.commit();
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Can't remove user from DB " + entity);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
