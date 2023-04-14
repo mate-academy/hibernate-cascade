d13f855e43b7daf206558314de5c77be271cc0b1
Artem Grunin <artem.grunin.2006@gmail.com>
2023-04-14 12:59:29 +0300

    fixed message

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -26,7 +26,7 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't create comment " + entity + " in DB ", e);
+            throw new RuntimeException("Can't create user " + entity + " in DB ", e);
         } finally {
             if (session != null) {
                 session.close();
