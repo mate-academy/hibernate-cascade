51a27e0ad9580e8ff1e00c25b9dadcf8656329a1
Mykyta Nikonets <nikita.nikonets@gmail.com>
2023-04-16 01:56:13 +0300

    implemented cascades for given classes

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import javax.persistence.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,57 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Long id = (Long) session.save(entity);
+            entity.setId(id);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to save comment to DB");
+        } finally {
+            session.close();
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        Session session = factory.openSession();
+        Comment comment = session.get(Comment.class, id);
+        session.close();
+        return comment;
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        Session session = factory.openSession();
+        String hql = "FROM Comment";
+        Query query = session.createQuery(hql);
+        List<Comment> comments = query.getResultList();
+        session.close();
+        return comments;
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to delete comment from DB");
+        } finally {
+            session.close();
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetailsDao {
     public MessageDetailsDaoImpl(SessionFactory sessionFactory) {
@@ -11,11 +13,29 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
 
     @Override
     public MessageDetails create(MessageDetails entity) {
-        return null;
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Long id = (Long) session.save(entity);
+            entity.setId(id);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to save message details to DB");
+        } finally {
+            session.close();
+        }
+        return entity;
     }
 
     @Override
     public MessageDetails get(Long id) {
-        return null;
+        Session session = factory.openSession();
+        MessageDetails messageDetails = session.get(MessageDetails.class, id);
+        session.close();
+        return messageDetails;
     }
 }
