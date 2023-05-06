23efc7e67491b992fb8d41fee413e041a6e4f845
Artur Muliukov <arthur.muliukov@gmail.com>
2023-05-06 17:19:55 +0200

    Solved 1.0

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -1,9 +1,13 @@
 package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDao;
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.Message;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +16,65 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
 
     @Override
     public Message create(Message entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can not create message: " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        Message message;
+        try (Session session = factory.openSession()) {
+            message = session.get(Message.class, id);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not get message by id: " + id, e);
+        }
+        return message;
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            Query<Message> messages = session.createQuery("from Message", Message.class);
+            return messages.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not get all messages", e);
+        }
     }
 
     @Override
     public void remove(Message entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can not remove message: " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
