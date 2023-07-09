d3a873266da7ca7fb69fda89a41407134cb05eae
Petro Kobevko <kobevkopi@gmail.com>
2023-07-09 18:53:48 +0200

    Added task solution

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,8 +1,27 @@
 package core.basesyntax.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "content")
     private String content;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "message_details_id")
     private MessageDetails messageDetails;
 
     public Long getId() {
--- src/main/java/core/basesyntax/util/HibernateUtil.java
+++ src/main/java/core/basesyntax/util/HibernateUtil.java
@@ -1,10 +1,10 @@
-package core.basesyntax;
+package core.basesyntax.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory = initSessionFactory();
+    private static final SessionFactory sessionFactory = initSessionFactory();
 
     private HibernateUtil() {
     }
