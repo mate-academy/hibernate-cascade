743f9df6d197826b9e8e571c2901fbb8c80598ef
Anton Uzhva <anton.itec91@gmail.com>
2023-08-03 13:33:41 +0300

    Initial commit

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,67 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not create comment", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            Comment comment = session.get(Comment.class, id);
+            return comment;
+        } catch (Exception e) {
+            throw new RuntimeException("Can not get comment with id" + id, e);
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            Query<Comment> getAllComments = session.createQuery("from Comment c", Comment.class);
+            return getAllComments.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can not get all comments", e);
+        }
+
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not remove entity with id" + entity.getId(), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
