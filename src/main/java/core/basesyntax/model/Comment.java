86d907fd0160ef144abff88051cb46136c659914
holsoni <soniakostashchuk@gmail,com>
2023-08-15 20:16:36 +0300

    fixed imports

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,7 +1,9 @@
 package core.basesyntax.model;
 
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,7 +15,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-    @OneToMany
+    @OneToMany(cascade = CascadeType.REFRESH)
     private List<Smile> smiles;
 
     public Long getId() {
