c6086b9c942002a25bf1f2bb6b66afca855de328
holsoni <soniakostashchuk@gmail,com>
2023-08-16 08:40:28 +0300

    Fixed cascades

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -24,7 +24,8 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't add message details {" + entity + "} to database!", ex);
+            throw new RuntimeException(
+                    "Can't add message details {" + entity + "} to database!", ex);
         } finally {
             if (session != null) {
                 session.close();
@@ -38,7 +39,8 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
         try (Session session = factory.openSession()) {
             return session.get(MessageDetails.class, id);
         } catch (Exception ex) {
-            throw new RuntimeException("Can't get message details from the DB with id " + id, ex);
+            throw new RuntimeException(
+                    "Can't get message details from the DB with id " + id, ex);
         }
     }
 }
