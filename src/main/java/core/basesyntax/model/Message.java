467f71ac774f5dd68f1bfbc52924c1390fd5c7b5
Volodymyr Mykychak <leadervlo.d@gmail.com>
2023-08-23 17:16:49 +0300

    commit

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,8 +1,23 @@
 package core.basesyntax.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToOne(fetch = FetchType.EAGER,
+            cascade = { CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE})
     private MessageDetails messageDetails;
 
     public Long getId() {
