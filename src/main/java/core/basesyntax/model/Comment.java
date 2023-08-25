c5465574c15ff7efbbd13995725f58eec8bdcad5
Yura Uniyat <yurauniyat123@gmail.com>
2023-08-25 14:52:28 +0300

    Homework hibernate-cascade. Added fix and files after mvn clean package.

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,6 +1,12 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.List;
 
 @Entity
