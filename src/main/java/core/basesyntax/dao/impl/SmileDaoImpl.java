e4da38ba27d65e5b2a2242d4c896867ae83b2f2f
Tetiana Koval <tetyanak0102@gmail.com>
2023-09-21 18:55:18 +0300

    implemented all dao classes, added cascade types for entities

--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -1,7 +1,11 @@
 package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.SmileDao;
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.Smile;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
 import java.util.List;
 import org.hibernate.SessionFactory;
 
@@ -11,17 +15,44 @@ public class SmileDaoImpl extends AbstractDao implements SmileDao {
     }
 
     @Override
-    public Smile create(Smile entity) {
-        return null;
+    public Smile create(Smile smile) {
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = factory.openSession();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(smile);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Failed to save a smile: " + smile, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+        return smile;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        try (EntityManager entityManager = factory.openSession()) {
+            return entityManager.find(Smile.class, id);
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to get a smile with id: " + id, e);
+        }
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        try (EntityManager entityManager = factory.openSession()) {
+            Query query = entityManager.createQuery("from Smile", Smile.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to get all smiles", e);
+        }
     }
 }
