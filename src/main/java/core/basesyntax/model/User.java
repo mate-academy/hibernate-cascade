0f629f8fa470e5ec1b93ebae56d5728f43dd6d8e
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-09-26 22:22:22 +0300

    fixed checkstyle

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,7 +1,12 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.util.List;
 
 @Entity
@@ -11,7 +16,7 @@ public class User {
     private Long id;
     private String username;
     @OneToMany(cascade = CascadeType.PERSIST,
-            fetch = FetchType.EAGER)
+                fetch = FetchType.EAGER)
     private List<Comment> comments;
 
     public Long getId() {
