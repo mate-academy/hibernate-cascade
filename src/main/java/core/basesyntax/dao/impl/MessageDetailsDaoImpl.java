c0cf4899d0eeb14317f903bf919cb87595f66aa3
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-09-27 15:37:38 +0300

    added cascades and implemented dao layer

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetailsDao {
@@ -11,11 +12,23 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
 
     @Override
     public MessageDetails create(MessageDetails entity) {
-        return null;
+        try {
+            return factory.fromTransaction(session -> {
+                session.persist(entity);
+                return entity;
+            });
+        } catch (Exception e) {
+            throw new RuntimeException("Can't save message details to DB: " + entity.getId(), e);
+        }
     }
 
     @Override
     public MessageDetails get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(MessageDetails.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Can't get an id of current message detail from DB: " + id, e);
+        }
     }
 }
