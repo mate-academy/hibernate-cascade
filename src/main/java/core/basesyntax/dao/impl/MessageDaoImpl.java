715965ba78ab719c934a9b32fae48923430cc541
Yura Novak <yuranovakr@gmail.com>
2023-10-22 12:57:02 +0300

    add solution hibernate-cascade.01

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.MessageDao;
 import core.basesyntax.model.Message;
 import java.util.List;
+import java.util.Objects;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +16,56 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
 
     @Override
     public Message create(Message entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            Objects.requireNonNull(transaction).rollback();
+            throw new RuntimeException("Can't add new Message to DB: " + entity);
+        } finally {
+            Objects.requireNonNull(session).close();
+        }
+        return entity;
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Message.class, id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get id of Message from DB");
+        }
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            Query<Message> getAllCommentQuery = session.createQuery(
+                    "from Message", Message.class);
+            return getAllCommentQuery.getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get all Messages from DB");
+        }
     }
 
     @Override
     public void remove(Message entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            Objects.requireNonNull(transaction).rollback();
+            throw new RuntimeException("Can't remove Message from DB: " + entity);
+        } finally {
+            Objects.requireNonNull(session).close();
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+import java.util.Objects;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +16,56 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            Objects.requireNonNull(transaction).rollback();
+            throw new RuntimeException("Can't add new User to DB: " + entity);
+        } finally {
+            Objects.requireNonNull(session).close();
+        }
+        return entity;
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(User.class, id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get id of User from DB: " + id);
+        }
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            Query<User> getAllCommentQuery = session.createQuery(
+                    "from User", User.class);
+            return getAllCommentQuery.getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get all Users from DB");
+        }
     }
 
     @Override
     public void remove(User entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            Objects.requireNonNull(transaction).rollback();
+            throw new RuntimeException("Can't remove User from DB: " + entity);
+        } finally {
+            Objects.requireNonNull(session).close();
+        }
     }
 }
