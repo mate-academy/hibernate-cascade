b2f3859e9d7837ea7cc738d648254b7e6cfeff31
testSouth <southparaplan@gmail.com>
2023-10-25 19:37:13 +0300

    implementation all classes

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +16,57 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not save in DB ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+
+            }
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            Comment comment = session.get(Comment.class, id);
+            Hibernate.initialize(comment.getSmiles());
+            return comment;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Can not find an object ", e);
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            String hql = "FROM Comment";
+            Query<Comment> query = session.createQuery(hql, Comment.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can not get All object ", e);
+        }
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Transaction transaction;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        }
     }
 }
