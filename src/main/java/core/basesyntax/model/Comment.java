14c0d463f8a02e25a48c8c95519e8f70817fcf37
Taras Nedaskivskyi <nadashkivskyitv@gmail.com>
2023-11-11 13:31:44 +0200

    task solution

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,16 +1,23 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.util.List;
 
+@Entity
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToMany
     private List<Smile> smiles;
+
+    public Comment() {
+    }
 
     public Long getId() {
         return id;
@@ -35,4 +42,13 @@ public class Comment {
     public void setSmiles(List<Smile> smiles) {
         this.smiles = smiles;
     }
+
+    @Override
+    public String toString() {
+        return "Comment{"
+                + "id=" + id
+                + ", content='" + content + '\''
+                + '}';
+    }
+
 }
--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,11 +1,21 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class MessageDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String sender;
     private LocalDateTime sentTime;
+
+    public MessageDetails() {
+    }
 
     public Long getId() {
         return id;
@@ -30,4 +40,14 @@ public class MessageDetails {
     public void setSentTime(LocalDateTime sentTime) {
         this.sentTime = sentTime;
     }
+
+    @Override
+    public String toString() {
+        return "MessageDetails{"
+                + "id=" + id
+                + ", sender='" + sender + '\''
+                + ", sentTime=" + sentTime
+                + '}';
+    }
+
 }
