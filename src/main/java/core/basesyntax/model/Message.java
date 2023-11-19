ec9e2338f7c8c615adf09fb137804794444e8a73
Bohdan Bilko <rapsbodik@gmail.com>
2023-11-19 18:18:32 +0200

    first solution

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,8 +1,23 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToOne (cascade = { CascadeType.PERSIST, CascadeType.REMOVE },
+            fetch = FetchType.LAZY)
     private MessageDetails messageDetails;
 
     public Long getId() {
