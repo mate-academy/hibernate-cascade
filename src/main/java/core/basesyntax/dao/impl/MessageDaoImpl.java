a7f0665629ad61440d1b90053d8a5ee202f7610b
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-11-21 15:24:35 +0200

    complete DaoImpl classes and model classes

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -3,30 +3,79 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.MessageDao;
 import core.basesyntax.model.Message;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
+    private static final String CANNOT_CREATE_MESSAGE_TEMPLATE = "Cannot create message: ";
+    private static final String CANNOT_GET_MESSAGE_TEMPLATE = "Cannot get message with id: ";
+    private static final String CANNOT_GET_MESSAGES = "Cannot get messages";
+    private static final String CANNOT_REMOVE_MESSAGE_TEMPLATE = "Cannot remove message: ";
+    private static final String GET_ALL_MESSAGES_QUERY = "FROM Message";
+
     public MessageDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Message create(Message entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(CANNOT_CREATE_MESSAGE_TEMPLATE + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Message.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException(CANNOT_GET_MESSAGE_TEMPLATE + id, e);
+        }
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery(GET_ALL_MESSAGES_QUERY, Message.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException(CANNOT_GET_MESSAGES, e);
+        }
     }
 
     @Override
     public void remove(Message entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(CANNOT_REMOVE_MESSAGE_TEMPLATE + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -3,25 +3,57 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
+    private static final String CANNOT_CREATE_SMILE_TEMPLATE = "Cannot create smile: ";
+    private static final String CANNOT_GET_SMILE_TEMPLATE = "Cannot get smile with id: ";
+    private static final String CANNOT_GET_SMILES = "Cannot get smiles";
+    private static final String GET_ALL_SMILES_QUERY = "FROM Smile";
+
     public SmileDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Smile create(Smile entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(CANNOT_CREATE_SMILE_TEMPLATE + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Smile.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException(CANNOT_GET_SMILE_TEMPLATE + id, e);
+        }
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery(GET_ALL_SMILES_QUERY, Smile.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException(CANNOT_GET_SMILES, e);
+        }
     }
 }
