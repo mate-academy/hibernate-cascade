a919c11a573e64a921da83d0966db4fdf3e0dcfc
Roman Globa <globaroman@gmail.com>
2023-11-28 21:53:23 +0200

    The solve task

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,30 +3,80 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
+
     public CommentDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+
+            if (transaction != null) {
+                transaction.rollback();
+                throw new RuntimeException("Can't create entity");
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Comment.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Not found entity with id " + id);
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            Query<Comment> fromComment = session.createQuery("FROM Comment", Comment.class);
+            return fromComment.list();
+        } catch (Exception e) {
+            throw new RuntimeException("Not found any objects");
+        }
     }
 
     @Override
     public void remove(Comment entity) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+
+            if (transaction != null) {
+                transaction.rollback();
+                throw new RuntimeException("Can't create entity");
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 
     }
 }
