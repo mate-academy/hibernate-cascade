63f0069ce0343739f0a0418c016a10347b4d0bec
IhorTrokhymchuk <hameleon009@gmail.com>
2023-11-30 22:25:49 +0200

    same add annotation and CommentDao

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,6 +1,13 @@
 package core.basesyntax.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
     private MessageDetails messageDetails;
