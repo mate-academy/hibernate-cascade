e846a4103c2f8e61b5cb272ae4ab89e1cdff2ca5
Oleksii <oleksii.makovkin@gmail.com>
2023-12-11 08:36:54 +0200

    hibernate-cascade

--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import java.util.Objects;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
     public SmileDaoImpl(SessionFactory sessionFactory) {
@@ -12,16 +15,39 @@ public class SmileDaoImpl extends AbstractDao implements SmileDao {
 
     @Override
     public Smile create(Smile entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            Objects.requireNonNull(transaction).rollback();
+            throw new RuntimeException("can't created entity from db", e);
+        } finally {
+            Objects.requireNonNull(session).close();
+        }
+        return entity;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Smile.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get id from db", e);
+        }
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            return session.createQuery("SELECT a FROM Smile a", Smile.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all smile from db", e);
+        }
     }
 }
