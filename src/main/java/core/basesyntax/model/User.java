9a7dc1d6aadf47b360e6ea97097893d4b1e25556
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-20 17:17:39 +0200

    added changes to the class User

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,7 +1,13 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
 import java.util.List;
 
 @Entity
