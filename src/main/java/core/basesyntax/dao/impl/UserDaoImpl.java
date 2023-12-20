4db4932239188868b974d5c00c730d3622196857
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-20 14:01:38 +0200

    added changes to the class UserDaoImpl

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,50 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t create entity: " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        Session session = factory.openSession();
+        return session.get(User.class, id);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        try (Session session = factory.openSession()){
+            User user = session.get(User.class, 1L);
+            return List.of(user);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t find user: ", e);
+        }
     }
 
     @Override
     public void remove(User entity) {
-
+        try (Session session = factory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new DataProcessingException("Error while removing user: ", e);
+        }
     }
 }
