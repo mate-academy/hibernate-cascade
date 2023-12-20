05438dba83acb4d2be879fcb4cf999f20d6abaea
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-20 17:17:56 +0200

    added changes to the class Smile

--- src/main/java/core/basesyntax/model/Smile.java
+++ src/main/java/core/basesyntax/model/Smile.java
@@ -1,6 +1,10 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "smiles")
