345bdfc3ae2afec96bc827000efdf2f3ae22dc27
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-20 17:18:19 +0200

    added changes to the class

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,7 +1,10 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import java.time.LocalDateTime;
 
 @Entity
