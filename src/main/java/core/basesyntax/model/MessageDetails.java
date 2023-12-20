237ce893cf97cfdce180e17042b1ed5a1e2355e2
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-20 17:28:47 +0200

    added chanegs to the class MessageDetails

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,10 +1,10 @@
 package core.basesyntax.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
