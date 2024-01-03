7ea5c1597212ce6e13adfd48bebc668d8bd04bd6
TkachukTara <zaz42988@gmail.com>
2024-01-03 13:21:15 +0200

    Done HW cascades!

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,30 +3,61 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
+    private static final String QUERY_GET_ALL_COMMENTS = "SELECT * FROM comments";
+
     public CommentDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        try (Session session = factory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.persist(entity);
+                transaction.commit();
+                return entity;
+            } catch (Exception e) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create Comment in DB" + entity.getId());
+        }
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Comment.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get comment from DB" + id);
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createNativeQuery(QUERY_GET_ALL_COMMENTS, Comment.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("can't get all comments from DB", e);
+        }
     }
 
     @Override
     public void remove(Comment entity) {
-
+        try (Session session = factory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.remove(entity);
+                transaction.commit();
+            } catch (Exception e) {
+                transaction.rollback();
+                throw new RuntimeException("Can't delete Comment from DB" + entity.getId());
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -3,25 +3,47 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
+    private static final String QUERY_GET_ALL_SMILES = "SELECT * FROM smiles";
+
     public SmileDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Smile create(Smile entity) {
-        return null;
+        try (Session session = factory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.persist(entity);
+                transaction.commit();
+                return entity;
+            } catch (Exception e) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create smile in DB" + entity.getId());
+        }
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Smile.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Cat't get smile from DB" + id);
+        }
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createNativeQuery(QUERY_GET_ALL_SMILES, Smile.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("can't get all smiles from DB", e);
+        }
     }
 }
