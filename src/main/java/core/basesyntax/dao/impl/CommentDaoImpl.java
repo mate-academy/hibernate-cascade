224617a721de768d82a4a0449afb1a2904ce4918
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-10 23:15:02 +0200

    Update CommentDaoImpl.java

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -2,31 +2,82 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
+import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
+
     public CommentDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                throw new RuntimeException("Can`t save comment " + entity + " to DB");
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        Comment comment = null;
+        try (Session session = factory.openSession()) {
+            comment = session.get(Comment.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t find comment with id = " + id + " in Db");
+        }
+        return comment;
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        List<Comment> list = new ArrayList<>();
+        try (Session session = factory.openSession()) {
+            Query<Comment> query = session.createQuery("FROM Comment", Comment.class);
+            list = query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t find any comments in Db");
+        }
+        return list;
     }
 
     @Override
     public void remove(Comment entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can`t remove comment " + entity + " from DB");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
