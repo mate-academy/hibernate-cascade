f91d52c60b5afa6e55e1fa2c2646c44b3e3e9dfd
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2024-02-10 23:24:05 +0200

    Update MessageDetails.java

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -16,18 +16,23 @@ public class MessageDetails {
     public Long getId() {
         return id;
     }
+    
     public void setId(Long id) {
         this.id = id;
     }
+    
     public String getSender() {
         return sender;
     }
+    
     public void setSender(String sender) {
         this.sender = sender;
     }
+    
     public LocalDateTime getSentTime() {
         return sentTime;
     }
+    
     public void setSentTime(LocalDateTime sentTime) {
         this.sentTime = sentTime;
     }
