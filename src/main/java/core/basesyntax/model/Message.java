977c08a649d0e68835a6d39a1776da4cc645e207
AdamMudrak <mudrets2000@gmail.com>
2024-02-21 13:56:31 +0200

    Add some of what I believe to be needed annotations(without Cascade types for now)

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,8 +1,18 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    @OneToOne
     private MessageDetails messageDetails;
 
     public Long getId() {
