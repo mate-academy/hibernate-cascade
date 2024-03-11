cc9de774cba23cc64873d974bd7451afd3331d25
shumYevh <shymeiko.zenya@gmail.com>
2024-03-11 18:49:04 +0200

    implementedCascade

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.MessageDao;
 import core.basesyntax.model.Message;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -11,22 +15,76 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
     }
 
     @Override
-    public Message create(Message entity) {
-        return null;
+    public Message create(Message message) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(message);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert Message to DB: " + message);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return message;
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        Message dbMessage = null;
+        try (Session session = factory.openSession()) {
+            dbMessage = session.get(Message.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get Message by id: " + id);
+        }
+        return dbMessage;
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        Session session = null;
+        List<Message> messages;
+        try {
+            session = factory.openSession();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Message> criteria = builder.createQuery(Message.class);
+            criteria.from(Message.class);
+            messages = session.createQuery(criteria).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all Messages from DB. ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return messages;
     }
 
     @Override
-    public void remove(Message entity) {
-
+    public void remove(Message message) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(message);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cant delete Message from DB: " + message, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -11,22 +15,76 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
     }
 
     @Override
-    public User create(User entity) {
-        return null;
+    public User create(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert User to DB: " + user);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        User dbUser = null;
+        try (Session session = factory.openSession()) {
+            dbUser = session.get(User.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get User by id: " + id);
+        }
+        return dbUser;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        Session session = null;
+        List<User> users;
+        try {
+            session = factory.openSession();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteria = builder.createQuery(User.class);
+            criteria.from(User.class);
+            users = session.createQuery(criteria).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all Users from DB. ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return users;
     }
 
     @Override
-    public void remove(User entity) {
-
+    public void remove(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cant delete User from DB: " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
