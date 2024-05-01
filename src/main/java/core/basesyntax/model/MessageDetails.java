55a3320e3ee60160e9388377956ec7173ca8c46a
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2024-05-02 00:30:58 +0300

    2

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,11 +1,11 @@
 package core.basesyntax.model;
 
-import java.time.LocalDateTime;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "message_details")
