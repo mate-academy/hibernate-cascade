c7fcf63db71f8a7685edcefdc3ea645b4c8a0860
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2024-05-02 00:37:38 +0300

    1

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,7 +1,7 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
