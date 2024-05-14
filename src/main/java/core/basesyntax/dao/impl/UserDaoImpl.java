680a73edd3a83eaaba98ad5a77b9f091d6a1e030
Vadym Stanishevskyi <mainvadym@gmail.com>
2024-05-14 13:06:42 +0200

    added necessary anotations for entities and implemented all DAD methods

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -2,6 +2,8 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
 import org.hibernate.SessionFactory;
 
@@ -12,21 +14,58 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.createEntityManager();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            entityManager.persist(entity);
+            entityTransaction.commit();
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Cannot create user: " + entity, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        EntityManager entityManager = factory.createEntityManager();
+        return entityManager.find(User.class, id);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        EntityManager entityManager = factory.createEntityManager();
+        return entityManager.createQuery("SELECT c FROM User c", User.class).getResultList();
     }
 
     @Override
     public void remove(User entity) {
-
+        EntityManager entityManager = null;
+        EntityTransaction entityTransaction = null;
+        try {
+            entityManager = factory.createEntityManager();
+            entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            entityManager.remove(entity);
+            entityTransaction.commit();
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+            }
+            throw new RuntimeException("Cannot remove User: " + entity, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
