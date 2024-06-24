4fcba9dfbcaae7b0da17f4e4ec8b5834c55bb441
Vadym <Servicepost.v@gmail.com>
2024-06-24 18:06:20 +0200

    Implements all dao methods. Added annotation to models.

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -1,9 +1,13 @@
 package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.CommentDao;
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import java.util.function.Consumer;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +16,52 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        executeInTransaction(session -> session.persist(entity));
+        return entity;
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Comment.class, id);
+        } catch (DataProcessingException e) {
+            throw new DataProcessingException("Comment not found with id: " + id);
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        List<Comment> comments;
+        try (Session session = factory.openSession()) {
+            comments = session.createQuery("from Comment").list();
+            return comments;
+        } catch (DataProcessingException e) {
+            throw new DataProcessingException("Comments was not found. " + e.getMessage());
+        }
     }
 
     @Override
     public void remove(Comment entity) {
+        executeInTransaction(session -> session.remove(entity));
+    }
 
+    private void executeInTransaction(Consumer<Session> action) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            action.accept(session);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Operation with comment not done. " + e.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
