885329628214ada12b46226c91e8451ad58ba776
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-10 10:51:05 +0300

    CommentDaoImpl.java

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -5,7 +5,6 @@ import core.basesyntax.model.Comment;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
 import java.util.List;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
