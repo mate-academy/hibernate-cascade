a6592782c9fd37aef2928866e24a68c2946283d9
Ivan Litvynets <Litvyvanya@gmail.com>
2024-07-12 00:09:16 +0200

    implemented hibernate cascades

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -2,8 +2,13 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +17,72 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            throw new RuntimeException("Cannot create a user : \"" + entity + "\"", e);
+        } finally {
+            closeSession(session);
+        }
+        return entity;
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(User.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot get a user with id: " + id, e);
+        }
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaQuery<User> criteriaQuery = session
+                    .getCriteriaBuilder().createQuery(User.class);
+            Root<User> rootEntry = criteriaQuery.from(User.class);
+            CriteriaQuery<User> all = criteriaQuery.select(rootEntry);
+            TypedQuery<User> allQuery = session.createQuery(all);
+            return allQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot get a user-list from DB");
+        }
     }
 
     @Override
     public void remove(User entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.remove(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            rollbackTransaction(transaction);
+            throw new RuntimeException("Cannot delete user : \"" + entity + "\" from DB", e);
+        } finally {
+            closeSession(session);
+        }
     }
+
+    private void rollbackTransaction(Transaction transaction) {
+        if (transaction != null) {
+            transaction.rollback();
+        }
+    }
+
+    private void closeSession(Session session) {
+        if (session != null) {
+            session.close();
+        }
+    }
+
 }
