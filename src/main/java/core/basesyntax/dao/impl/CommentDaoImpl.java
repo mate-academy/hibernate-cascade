f2497e70166ad987efeab73960f5e532f57d4469
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-07-28 19:24:16 +0300

    Hibernate cascade task solution

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +14,74 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            session.persist(entity);
+            transaction.commit();
+            return entity;
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add Comment object in database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        Comment comment = null;
+
+        try (Session session = factory.openSession()) {
+            comment = session.get(Comment.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't retrieve Comment object by id:" + id, e);
+        }
+
+        return comment;
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        List<Comment> commentList = null;
+
+        try (Session session = factory.openSession()) {
+            commentList = session.createQuery("FROM Comment", Comment.class).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't retrieve all Comment objects:", e);
+        }
+
+        return commentList;
     }
 
     @Override
     public void remove(Comment entity) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
 
+            session.remove(entity);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove Comment object from database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.MessageDao;
 import core.basesyntax.model.Message;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +14,74 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
 
     @Override
     public Message create(Message entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            session.persist(entity);
+            transaction.commit();
+            return entity;
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add Message object in database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        Message message = null;
+
+        try (Session session = factory.openSession()) {
+            message = session.get(Message.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't retrieve Message object by id:" + id, e);
+        }
+
+        return message;
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        List<Message> messageList = null;
+
+        try (Session session = factory.openSession()) {
+            messageList = session.createQuery("FROM Message", Message.class).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't retrieve all Comment objects:", e);
+        }
+
+        return messageList;
     }
 
     @Override
     public void remove(Message entity) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
 
+            session.remove(entity);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove Message object from database", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
