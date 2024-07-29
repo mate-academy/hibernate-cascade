aca8483e303aa6cd1401442f882345ebb28880d2
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-29 08:02:08 +0300

    UserDao.java

--- src/main/java/core/basesyntax/dao/UserDao.java
+++ src/main/java/core/basesyntax/dao/UserDao.java
@@ -1,7 +1,64 @@
-package core.basesyntax.dao;
+package core.basesyntax.dao.impl;
 
+import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-public interface UserDao extends GenericDao<User> {
+import java.util.List;
 
+public class UserDaoImpl extends AbstractDao implements UserDao {
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public User create(User entity) {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert User entity", e);
+        }
+    }
+
+    @Override
+    public User get(Long id) {
+        try (Session session = factory.openSession()) {
+            return session.get(User.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get User entity by id", e);
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = factory.openSession()) {
+            return session.createQuery("from User", User.class).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all User entities", e);
+        }
+    }
+
+    @Override
+    public void remove(User entity) {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't delete User entity", e);
+        }
+    }
 }
