d72b57d33e0385bdaea573974bafa2639722d2b8
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-29 07:53:14 +0300

    Smile.java

--- src/main/java/core/basesyntax/model/Smile.java
+++ src/main/java/core/basesyntax/model/Smile.java
@@ -1,9 +1,6 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Smile {
