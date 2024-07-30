5eef1b8ab47d94e3d7fffbd6849a911fa4b0ef1a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-30 07:36:21 +0300

    Message.java

--- src/main/java/core/basesyntax/model/Message.java
+++ src/main/java/core/basesyntax/model/Message.java
@@ -1,5 +1,6 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
