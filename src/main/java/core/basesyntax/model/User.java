c41f661a0da964fd32d99eb1c5a0d644c88c7590
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-08-06 07:47:12 +0300

    User.java

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,10 +1,17 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.*;
 import java.util.List;
 
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
     private List<Comment> comments;
 
     public Long getId() {
