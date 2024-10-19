c80feba40a32825a313e54fae306644619a0be2e
Oleksandr Prots <sashaproc2289@gmail.com>
2024-10-19 17:00:19 +0300

    hibernate-cascade

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -2,8 +2,11 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.CommentDao;
 import core.basesyntax.model.Comment;
+import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +15,75 @@ public class CommentDaoImpl extends AbstractDao implements CommentDao {
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            if (entity.getSmiles() != null) {
+                for (Smile smile : entity.getSmiles()) {
+                    Smile existingSmile = session.get(Smile.class, smile.getId());
+                    if (existingSmile == null) {
+                        throw new RuntimeException("Smile with id "
+                                + smile.getId()
+                                + " does not exist");
+                    }
+                }
+            }
+            session.persist(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to save comment: " + entity);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        Session session = null;
+        try {
+            session = factory.openSession();
+            return session.get(Comment.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get comment with id: " + id);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery("FROM Comment", Comment.class).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get all comments", e);
+        }
     }
 
     @Override
     public void remove(Comment entity) {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
 
+            session.remove(entity);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to remove comment", e);
+        }
     }
 }
