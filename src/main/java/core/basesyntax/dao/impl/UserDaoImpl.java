c5c5d15b8888e088f14aff1a3e6d0f3195c0be88
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-10-29 20:05:08 +0200

    developed implementations

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -2,31 +2,33 @@ package core.basesyntax.dao.impl;
 
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import org.hibernate.SessionFactory;
 
-public class UserDaoImpl extends AbstractDao implements UserDao {
+@Transactional
+public class UserDaoImpl extends AbstractDao<User> implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public User create(User entity) {
-        return null;
+        return super.create(entity);
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        return super.get(User.class, id);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return super.getAll(User.class);
     }
 
     @Override
     public void remove(User entity) {
-
+        super.remove(entity);
     }
 }
