8fa4e4c89f5c9ce7841e8d83308a4e89921c46e2
Mateusz Seler <mateusz.seler@gmail.com>
2024-11-29 12:49:17 +0100

    first version

--- src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDetailsDaoImpl.java
@@ -1,8 +1,11 @@
 package core.basesyntax.dao.impl;
 
+import core.basesyntax.DataProcessingException;
 import core.basesyntax.dao.MessageDetailsDao;
 import core.basesyntax.model.MessageDetails;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetailsDao {
     public MessageDetailsDaoImpl(SessionFactory sessionFactory) {
@@ -10,12 +13,52 @@ public class MessageDetailsDaoImpl extends AbstractDao implements MessageDetails
     }
 
     @Override
-    public MessageDetails create(MessageDetails entity) {
-        return null;
+    public MessageDetails create(MessageDetails messageDetails) {
+        if (messageDetails == null) {
+            throw new RuntimeException("unacceptable data");
+        }
+
+        if (messageDetails.getId() != null && get(messageDetails.getId()) != null) {
+            throw new RuntimeException(messageDetails + "already existed in database");
+        }
+
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(messageDetails);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("adding " + messageDetails + " into database failed");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return messageDetails;
     }
 
     @Override
     public MessageDetails get(Long id) {
-        return null;
+        Session session = null;
+        MessageDetails messageDetails = null;
+        try {
+            session = factory.openSession();
+            messageDetails = session.get(MessageDetails.class, id);
+        } catch (Exception e) {
+
+            throw new DataProcessingException("getting messageDetails with "
+                    + id + " from database failed");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return messageDetails;
     }
 }
--- src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/SmileDaoImpl.java
@@ -1,9 +1,12 @@
 package core.basesyntax.dao.impl;
 
+import core.basesyntax.DataProcessingException;
 import core.basesyntax.dao.SmileDao;
 import core.basesyntax.model.Smile;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class SmileDaoImpl extends AbstractDao implements SmileDao {
     public SmileDaoImpl(SessionFactory sessionFactory) {
@@ -11,17 +14,59 @@ public class SmileDaoImpl extends AbstractDao implements SmileDao {
     }
 
     @Override
-    public Smile create(Smile entity) {
-        return null;
+    public Smile create(Smile smile) {
+        if (smile == null) {
+            throw new RuntimeException("unacceptable data");
+        }
+
+        if (smile.getId() != null && get(smile.getId()) != null) {
+            throw new RuntimeException(smile + "already existed in database");
+        }
+
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(smile);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("adding " + smile + " into database failed");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return smile;
     }
 
     @Override
     public Smile get(Long id) {
-        return null;
+        Session session = null;
+        Smile smile = null;
+        try {
+            session = factory.openSession();
+            smile = session.get(Smile.class, id);
+        } catch (Exception e) {
+
+            throw new DataProcessingException("getting smile with "
+                    + id + " from database failed");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return smile;
     }
 
     @Override
     public List<Smile> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery("from Smile", Smile.class).getResultList();
+        }
     }
 }
