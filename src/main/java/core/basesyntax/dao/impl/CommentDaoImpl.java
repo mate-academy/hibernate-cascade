57d3523b36aedc2788a2111e0467132152a60ee2
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-12-02 19:36:24 +0100

    1

--- src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/CommentDaoImpl.java
@@ -1,28 +1,70 @@
 package core.basesyntax.dao.impl;
 
+import core.basesyntax.HibernateUtil;
 import core.basesyntax.dao.CommentDao;
+import core.basesyntax.exeption.DataProcessingException;
 import core.basesyntax.model.Comment;
+
 import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.management.Query;
 
 public class CommentDaoImpl extends AbstractDao implements CommentDao {
     public CommentDaoImpl(SessionFactory sessionFactory) {
+
         super(sessionFactory);
     }
 
     @Override
     public Comment create(Comment entity) {
-        return null;
+
+        if (entity == null) {
+            throw new RuntimeException("unacceptable data");
+        }
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can not add Comment", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Comment get(Long id) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        try {
+            Comment comment = session.get(Comment.class, id);
+            return comment;
+        } catch (Exception e) {
+            throw new DataProcessingException("Unable to get Comment with id: " + id, e);
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<Comment> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery("from Comment", Comment.class).getResultList();
+        }
     }
 
     @Override
--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -7,16 +7,19 @@ import org.hibernate.SessionFactory;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
+
         super(sessionFactory);
     }
 
     @Override
     public Message create(Message entity) {
+
         return null;
     }
 
     @Override
     public Message get(Long id) {
+
         return null;
     }
 
