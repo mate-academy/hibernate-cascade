ec7161e6bcde326ccf0d57776e0ebe38f4eac3be
Alinka Lyn <alinka.lyn@ukr.net>
2025-01-28 23:47:15 +0200

    solved task

--- src/main/java/core/basesyntax/model/Smile.java
+++ src/main/java/core/basesyntax/model/Smile.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "smile")
 public class Smile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,3 +45,4 @@ public class Smile {
                 + '}';
     }
 }
+
