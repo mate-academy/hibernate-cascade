eabcc2d243741edfcd0d26c2b3e945ee1f323968
FroGitHub <andrey8102310@gmail.com>
2025-02-06 16:19:58 +0200

    implement all and add annotations with cascades

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +14,110 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            if (!(entity.getComments() == null)) {
+                Session finalSession = factory.openSession();
+                entity.getComments().forEach(finalSession::saveOrUpdate);
+            }
+            session.save(entity);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("User create excp", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public User get(Long id) {
-        return null;
+
+        Session session = null;
+        Transaction transaction = null;
+
+        User user;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            user = session.get(User.class, id);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("User get excp");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+
+        List<User> user;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            user = session.createQuery("FROM User", User.class).list();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("User getAll excp");
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     @Override
     public void remove(User entity) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            session.remove(entity);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("User remove excp", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 
     }
 }
