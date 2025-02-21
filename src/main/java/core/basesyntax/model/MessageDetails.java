fb3ae126f32bb89a17385e53766038b88b30d7c0
eulu <anegamega@gmail.com>
2025-02-21 10:47:04 +0200

    Add hibernate annotations to the MessageDetails model

--- src/main/java/core/basesyntax/model/MessageDetails.java
+++ src/main/java/core/basesyntax/model/MessageDetails.java
@@ -1,8 +1,14 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "message_details")
 public class MessageDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String sender;
     private LocalDateTime sentTime;
