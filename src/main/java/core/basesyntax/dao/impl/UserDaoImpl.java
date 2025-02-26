ba113fe4bac1cacb6270c158237156f9be270cdf
Kate Kraska <kate2002.kk42@gmail.com>
2025-02-26 13:00:45 +0200

    fix problem 7

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,6 +3,8 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -36,10 +38,20 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User get(Long id) {
-        try (Session session = factory.openSession()) {
-            return session.get(User.class, id);
+        Session session = null;
+        try {
+            session = factory.openSession();
+            User user = session.get(User.class, id);
+            if (user != null) {
+                Hibernate.initialize(user.getComments());
+            }
+            return user;
         } catch (RuntimeException e) {
             throw new RuntimeException("Can't remove entity " + id, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
@@ -59,8 +71,12 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.remove(entity);
-            session.getTransaction().commit();
+            User managedEntity = session.get(User.class, entity.getId());
+            if (managedEntity != null) {
+                Hibernate.initialize(managedEntity.getComments());
+                session.remove(entity);
+                session.getTransaction().commit();
+            }
         } catch (RuntimeException e) {
             if (transaction != null) {
                 transaction.rollback();
