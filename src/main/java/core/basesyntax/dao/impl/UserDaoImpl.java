a8f7fa1be13884e82186b65afbecbc9244d70746
Kate Kraska <kate2002.kk42@gmail.com>
2025-02-26 13:48:00 +0200

    fix problem 8

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,7 +3,6 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -37,20 +36,10 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User get(Long id) {
-        Session session = null;
-        try {
-            session = factory.openSession();
-            User user = session.get(User.class, id);
-            if (user != null) {
-                Hibernate.initialize(user.getComments());
-            }
-            return user;
+        try (Session session = factory.openSession()) {
+            return session.get(User.class, id);
         } catch (RuntimeException e) {
-            throw new RuntimeException("Can't remove entity " + id, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+            throw new RuntimeException("Can't get user by id " + id, e);
         }
     }
 
@@ -72,10 +61,9 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
             transaction = session.beginTransaction();
             User managedEntity = session.get(User.class, entity.getId());
             if (managedEntity != null) {
-                Hibernate.initialize(managedEntity.getComments());
-                session.remove(entity);
-                session.getTransaction().commit();
+                session.remove(managedEntity);
             }
+            session.getTransaction().commit();
         } catch (RuntimeException e) {
             if (transaction != null) {
                 transaction.rollback();
--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -2,6 +2,7 @@ package core.basesyntax.model;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,7 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
-    @ManyToMany(cascade = CascadeType.PERSIST)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     private List<Comment> comments;
 
     public Long getId() {
