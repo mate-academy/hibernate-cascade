a039112a1f44f5ceba53b44d282d957fc4b897b4
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2025-03-17 15:42:52 +0100

    first_try

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -1,10 +1,23 @@
 package core.basesyntax.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @OneToMany
+    @JoinColumn(name = "comment_id")
     private List<Comment> comments;
 
     public Long getId() {
