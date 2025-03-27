3a74c4fb7f55a46c1def84c0718af187913c456e
Geks <gekatonheyr@ukr.net>
2025-03-27 20:49:07 +0200

    Had worked under hibernate cascade homework: - implemented all DAO interfaces; - described dependencies between Comment and Smile, User and Comment, Message and MessageDetails classes due to exercise requirements; -passed all tests.

--- src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/MessageDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.MessageDao;
 import core.basesyntax.model.Message;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MessageDaoImpl extends AbstractDao implements MessageDao {
     public MessageDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +14,76 @@ public class MessageDaoImpl extends AbstractDao implements MessageDao {
 
     @Override
     public Message create(Message entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Something is wrong with writing the MESSAGE: "
+                    + System.lineSeparator()
+                    + entity
+                    + System.lineSeparator()
+                    + " row to database. Closing connection and rolling back the transaction.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 
     @Override
     public Message get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.get(Message.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Something is wrong with getting data from DB for MESSAGE."
+                            + "Maybe such ID as: " + id + " is absent in corresponding DB table",
+                    e);
+        }
     }
 
     @Override
     public List<Message> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery("select a from messages a", Message.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Something is wrong with getting ALL the data from DB for MESSAGE."
+                            + "Maybe corresponding table is corrupted", e);
+        }
     }
 
     @Override
     public void remove(Message entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.delete(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Something is wrong with removing the MESSAGE: "
+                    + System.lineSeparator()
+                    + entity
+                    + System.lineSeparator()
+                    + " row from database. Closing connection and rolling back the transaction.",
+                    e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
