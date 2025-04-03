5afeef84ddadae9965a70b3f1bbbbe7b7cdc71d6
Olha <olha.oleshchuk@gmail.com>
2025-04-03 23:29:32 +0300

    remove lib and edit hibernate.cfg.xml  - eight try

--- src/main/java/core/basesyntax/lib/Dao.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.lib;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Dao {
-}
