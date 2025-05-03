f48ec8a6bc27baef47ab6c95f9e35fec2952c708
Volikkk <illavolanuk02@gmail.com>
2025-05-03 23:25:24 +0300

    done tasks

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.impl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class UserDaoImpl extends AbstractDao implements UserDao {
     public UserDaoImpl(SessionFactory sessionFactory) {
@@ -12,21 +14,81 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
 
     @Override
     public User create(User entity) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not save User object with data: "
+                    + entity + " to Db", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public User get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery(
+                            "select u from User u "
+                                    + "left join fetch u.comments "
+                                    + "where u.id = :id", User.class)
+                    .setParameter("id", id)
+                    .uniqueResult();
+        } catch (Exception e) {
+            throw new RuntimeException("Can not get User object with id: "
+                    + id + " from DB", e);
+        }
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        try (Session session = factory.openSession()) {
+            return session.createQuery(
+                            "select distinct u from User u "
+                                    + "left join fetch u.comments", User.class)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can not get list of User objects from DB", e);
+        }
     }
 
     @Override
     public void remove(User entity) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            User managedUser = session.createQuery(
+                            "select u from User u "
+                                    + "left join fetch u.comments "
+                                    + "where u.id = :id", User.class)
+                    .setParameter("id", entity.getId())
+                    .uniqueResult();
+            if (managedUser != null) {
+                session.remove(managedUser);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not remove User object with data: "
+                    + entity + " from DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
