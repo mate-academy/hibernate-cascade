d6b1c0aa27140eee10504238f48686b3083af528
Yaroslav Narozhnyi <gnredit9@gmail.com>
2025-05-05 16:16:13 +0300

    n3 import

--- src/main/java/core/basesyntax/model/Comment.java
+++ src/main/java/core/basesyntax/model/Comment.java
@@ -1,7 +1,7 @@
 package core.basesyntax.model;
 
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
